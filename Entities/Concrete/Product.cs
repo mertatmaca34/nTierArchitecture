645adb532fa55feb32387a0d03b3c4bd5be42e35
Mert Atmaca <mertatmaca34@gmail.com>
2023-06-12 12:59:49 +0300

    ++

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Abstract;
 
 namespace Entities.Concrete
 {
-    public class Product
+    public class Product : IEntity
     {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public short UnitInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string ProductName { get; set; }
     }
 }
