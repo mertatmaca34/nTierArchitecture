ef9d5e89f06c96f2531b11441b61c340f6bbad07
Mert Atmaca <mertatmaca34@gmail.com>
2023-06-13 08:33:39 +0300

    ++

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,16 +1,11 @@
-﻿using Entities.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Core.Entities;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : IEntity, new()
     {
-        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,58 +1,8 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
-    {
-        public void Add(Product entity)
-        {
-            using (SampleContext context = new SampleContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (SampleContext context = new SampleContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (SampleContext context = new SampleContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (SampleContext context = new SampleContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (SampleContext context = new SampleContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-    }
+    public class EfProductDal : EfEntityRepositoryBase<Product, SampleContext>, IProductDal { }
 }
