54dabc9562586cc0e49efe99e1bc68554de30dcc
Mert Atmaca <mertatmaca34@gmail.com>
2023-06-13 09:27:14 +0300

    ++

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,8 +1,28 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product, SampleContext>, IProductDal { }
+    public class EfProductDal : EfEntityRepositoryBase<Product, SampleContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (var context = new SampleContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 CategoryName = c.CategoryName,
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 UnitsInStock = p.UnitInStock
+                             };
+                return result.ToList();
+            }
+        }
+    }
 }
