b98eac5bb63a84219701d01475773376b6d8cc38
Mert Atmaca <mertatmaca34@gmail.com>
2023-06-13 11:01:01 +0300

    ++

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Business.Constants
+﻿namespace Business.Constants
 {
     public static class Messages
     {
         public static readonly string ProductAdded = "Ürün eklendi";
         public static readonly string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static readonly string MaintenanceTime = "Sistem bakımda";
+        public static readonly string ProductListed = "Ürünler listelendi";
     }
 }
